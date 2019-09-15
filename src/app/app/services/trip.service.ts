dc763486c4e2ee36a204c322188cffbe628457ff
Leonardo <leonardo.capoccia@hotmail.it>
2019-09-15 14:33:11 +0200

    Add trip service

---
+++ src/app/app/services/trip.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Trip } from '../models/trip';
+import { Page } from '../models/page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripService {
+
+  private BASE_URL = `${environment.TRIP_PLANNER_SERVICE_HOST}/api/trip`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getTrips(page: number, pageSize: number): Observable<Page<Trip>> {
+    return this.http.get<Page<Trip>>(`${this.BASE_URL}/${page}/${pageSize}`);
+  }
+
+  delete(trip: Trip): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.BASE_URL}/${trip.id}`);
+  }
+}
